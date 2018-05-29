db004003debb6a6f0377352fc7da4a77a666eec5
JohnInWI <psigncrafters@gmail.com>
2018-05-29 08:56:25 -0500

    Commit of docs
    Commit of docs

---
+++ snipsGreen.js
@@ -0,0 +1,25 @@
+snipsGreen([
+{
+"display": "btn_ontouch",
+"snip": `
+
+function btn_OnTouch()
+{
+//Show a popup message.
+app.ShowPopup( "Hello World!" );
+//Vibrate phone with a pattern (in milliseconds).
+//pause,vibrate,pause,vibrate...
+app.Vibrate( "0,100,30,100,50,300" );
+}
+
+`
+},
+{
+"display": "if Statement",
+"snip": "snippy2"
+},
+{
+"display": "snip3",
+"snip": "snippy3"
+}
+]); 
---
+++ testy.js
@@ -0,0 +1,25 @@
+snipperBlue([
+{
+"display": "btn_ontouch",
+"snip": `
+
+function btn_OnTouch()
+{
+//Show a popup message.
+app.ShowPopup( "Hello World!" );
+//Vibrate phone with a pattern (in milliseconds).
+//pause,vibrate,pause,vibrate...
+app.Vibrate( "0,100,30,100,50,300" );
+}
+
+`
+},
+{
+"display": "snip2",
+"snip": "snippy2"
+},
+{
+"display": "snip3",
+"snip": "snippy3"
+}
+]); 
